40447a69bde541612f517925bc2bb07f79ce194e
Eugene Bakaev <eugene.bakaev@gmail.com>
2026-05-05 18:55:14 +0300

    Convert configuration files to TypeScript

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -10,3 +11,4 @@ export default {
   plugins: [],
 }
 
+export default config
